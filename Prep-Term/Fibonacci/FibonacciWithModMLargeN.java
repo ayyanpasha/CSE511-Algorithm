4a4e7c3bee5009e07099e5ff144aa090ad1e0a0b
Ayyan Pasha <ayyanpasha@Armaans-MacBook-Pro.local>
2024-08-04 17:05:39 +0530

    Some changes

--- Prep-Term/Fibonacci/FibonacciWithModMLargeN.java
+++ Prep-Term/Fibonacci/FibonacciWithModMLargeN.java
@@ -1,17 +1,27 @@
 import java.util.*;
-//TC O(MOD)
-//SC O(M)
-class FibonacciWithModM {
+//TC O(MOD + logN)
+//SC O(M + logN)
+class FibonacciWithModMLargeN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter MOD value: ");
         int MOD = sc.nextInt();
-        System.out.print("Enter N to find Fibonacci(N): ");
-        int n = sc.nextInt();
+        System.out.print("Enter N to find Fibonacci(N) (Can be greater than 2^32): ");
+        int n = sc.next();
         int fib[] = new int[6*MOD+3];
         fib[1] = 1;
         int p = findP(fib,MOD);
-        System.out.print(fib[n%p]);
+        int nModp = findMOD(n,p);
+        System.out.print(fib[nModp]);
+    }
+    public static int findMOD(String n, int p){
+        char[] numbers = n.toCharArray();
+        int r = 0;
+        for(char number:numbers){
+            int current = number-'0';
+            r = (10*r+current)%p;
+        }
+        return r;
     }
     public static int findP(int fib[],int MOD){
         int i=2;
