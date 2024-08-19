ffedcf9d970f823e123e7cf82865b0dff4c8e323
Ayyan Pasha <ayyanpasha@Armaans-MacBook-Pro.local>
2024-08-19 11:34:18 +0530

    Optimized P_30 & P_31

--- Practice-Problem-1/P_30_ai_plus_aj_equal_ak.java
+++ Practice-Problem-1/P_30_ai_plus_aj_equal_ak.java
@@ -1,34 +1,33 @@
 import java.util.*;
 public class HelloWorld {
-    public static class Pair{
-        int i;
-        int j;
-        public Pair(int i, int j){
-            this.i = i;
-            this.j = j;
-        }
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
         for(int i=0;i<n;i++) arr[i] = sc.nextInt();
-        HashMap<Integer,Set<Pair>> map = new HashMap<>();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int sum = arr[i]+arr[j];
-                if(!map.containsKey(sum)) map.put(sum, new HashSet<Pair>());
-                map.get(sum).add(new Pair(i,j));
+        System.out.println(pairsExist(arr));
+    }
+    public static boolean pairsExist(int arr[]){
+        int n = arr.length;
+        for(int k=0;k<n;k++){
+            int i = 0;
+            int j = n-1;
+            while(i<j){
+                if(i == k){
+                    i++; continue;
+                }
+                if(j == k){
+                    j--; continue;
+                }
+
+                if(arr[i]+arr[j] == arr[k]) return true;
+                if(arr[i]+arr[j] > arr[k]){
+                    j--;
+                }else{
+                    i++;
+                }
             }
         }
-        List<List<Integer>> result = new ArrayList<>();
-        for(int k = 0;k<n;k++){
-            if(!map.containsKey(arr[k])) continue;
-            for(Pair current:map.get(arr[k])){
-                if(current.i == k || current.j == k) continue;
-                result.add(Arrays.asList(current.i, current.j, k));
-            }
-        }
-        System.out.println(result);
+        return false;
     }
 }
--- Practice-Problem-1/P_31_ai_minus_2aj_equal_ak_minus_3al.java
+++ Practice-Problem-1/P_31_ai_minus_2aj_equal_ak_minus_3al.java
@@ -1,41 +1,42 @@
 //a[i]-2a[j]=a[k]-3a[l]
+//a[i]+3a[l] = a[k]+2a[j];
 
 import java.util.*;
 public class P_31_ai_minus_2aj_equal_ak_minus_3al {
-    public static class Pair{
-        int i;
-        int j;
-        public Pair(int i, int j){
-            this.i = i;
-            this.j = j;
-        }
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
         for(int i=0;i<n;i++) arr[i] = sc.nextInt();
-        HashMap<Integer,Set<Pair>> map = new HashMap<>();
+        System.out.println(pairsExist(arr));
+    }
+    public static boolean pairsExist(int arr[]){
+        int n = arr.length;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i == j) continue;
-                int sum = arr[i]-2*arr[j];
-                if(!map.containsKey(sum)) map.put(sum, new HashSet<Pair>());
-                map.get(sum).add(new Pair(i,j));
-            }
-        }
-        List<List<Integer>> result = new ArrayList<>();
-        for(int k=0;k<n;k++){
             for(int l=0;l<n;l++){
-                if(k == l) continue;
-                int sum = arr[k]-3*arr[l];
-                if(!map.containsKey(sum)) continue;
-                for(Pair current:map.get(sum)){
-                    if(current.i == k || current.j == k || current.i == l || current.j == l) continue;
-                    result.add(Arrays.asList(current.i,current.j,k,l));
+                if(i == l) continue;
+                int k = 0;
+                int j = n-1;
+                int sum  = arr[i]+3*arr[l];
+                while(k<j){
+                    if(k == i || k == l){
+                        k++;
+                        continue;
+                    }
+                    if(j == i || j == l){
+                        j--;
+                        continue;
+                    }
+                    int currentSum = arr[k]+2*arr[j];
+                    if(sum == currentSum) return true;
+                    else if(sum > currentSum){
+                        k++;
+                    }else{
+                        j--;
+                    }
                 }
             }
         }
-        System.out.println(result);
+        return false;
     }
 }
